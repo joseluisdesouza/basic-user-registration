c710abce54262255bca756d87b7fd731f02c6a35
josesouza <joseluisdesouzaa@gmail.com>
2021-09-04 14:51:49 -0300

    initial commit

---
+++ src/main/java/com/backend/basicregistration/BasicRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.backend.basicregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicRegistrationApplication.class, args);
+	}
+
+}
