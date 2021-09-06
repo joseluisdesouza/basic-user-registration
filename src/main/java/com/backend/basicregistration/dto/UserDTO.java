86ca7edafb1db1241c0d7103bc4d2380cba9118a
josesouza <joseluisdesouzaa@gmail.com>
2021-09-05 22:51:03 -0300

    add anotation NotNull/NotEmpty/Positive

--- src/main/java/com/backend/basicregistration/dto/UserDTO.java
+++ src/main/java/com/backend/basicregistration/dto/UserDTO.java
@@ -1,8 +1,11 @@
 package com.backend.basicregistration.dto;
 
 import com.backend.basicregistration.entity.User;
+import com.sun.istack.NotNull;
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 
 @Data
@@ -12,7 +15,9 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class UserDTO {
 
+    @NotNull @Positive
     private Long id;
+    @NotNull @NotEmpty
     private String name;
     private LocalDate birthday;
     private String photo;
